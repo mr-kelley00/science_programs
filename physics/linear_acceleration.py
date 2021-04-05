335196edd16076e64b6c39a38c495807cc997bb5
Ryan Kelley <kelleyr@duvalschools.org>
2021-04-05 10:25:27 -0400

    Update linear_acceleration.py

--- physics/linear_acceleration.py
+++ physics/linear_acceleration.py
@@ -1,1 +1,6 @@
 # Linear Acceleration Calculator, Ryan Kelley, 04/05/21, 10:10AM, Version 0.0 
+
+velocity_initial = 0.0 # Default unit is meters per second. 
+velocity_final = 0.0 # Default unit is meters per second. 
+time = 0.0 # Default unit is seconds. 
+
